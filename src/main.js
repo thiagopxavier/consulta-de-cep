647ccb40de4fd6f1f16408869f4fce868c0d9ce2
Thiago Xavier <thiagox58@gmail.com>
2022-11-11 15:31:57 -0300

    first code

---
+++ src/main.js
@@ -0,0 +1,25 @@
+const cep = document.querySelector('#cep');
+
+const inputs = ['logradouro', 'bairro', 'localidade', 'uf']
+const showData = (result) => {
+  for(const data in result) {
+    if(document.querySelector(`#${data}`)){
+      document.querySelector(`#${data}`).value = result[data]
+    }
+  }
+}
+
+cep.addEventListener('input', (e) => {  // 'blur'
+  let search = cep.value.replace('-', '');
+  const option = {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'default'
+  }
+
+  fetch(`https://viacep.com.br/ws/${search}/json/`, option)
+  .then(response => response.json())
+  .then(result => showData(result))
+  .catch(e => console.log('Deu erro' + e.message))
+
+})
